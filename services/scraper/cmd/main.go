4be37bde1f458dd5019ac31300b80904f990b83e
arseniizyk <arseniizyk@gmail.com>
2025-09-14 16:51:27 +0300

    scraper: added logger level to main.go fixed ports for new config logic removed useless App struct fields changed logs now app.go only init repos and usecases, instead of database

--- services/scraper/cmd/main.go
+++ services/scraper/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"log/slog"
 
 	"github.com/arseniizyk/mgkct-schedule-bot/services/scraper/internal/app"
 )
@@ -11,6 +12,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	slog.SetLogLoggerLevel(slog.LevelDebug)
 
 	if err := app.Run(); err != nil {
 		log.Fatal(err)
--- services/scraper/internal/app/app.go
+++ services/scraper/internal/app/app.go
@@ -2,8 +2,6 @@ package app
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"log/slog"
 	"net"
 	"os"
@@ -14,39 +12,42 @@ import (
 	pb "github.com/arseniizyk/mgkct-schedule-bot/libs/proto"
 
 	parserUC "github.com/arseniizyk/mgkct-schedule-bot/services/scraper/pkg/parser/usecase"
+	scheduleRepo "github.com/arseniizyk/mgkct-schedule-bot/services/scraper/pkg/schedule/repository/postgres"
 	scheduleUC "github.com/arseniizyk/mgkct-schedule-bot/services/scraper/pkg/schedule/usecase"
 
 	"github.com/arseniizyk/mgkct-schedule-bot/services/scraper/internal/config"
 	"github.com/arseniizyk/mgkct-schedule-bot/services/scraper/internal/database"
-	"github.com/arseniizyk/mgkct-schedule-bot/services/scraper/internal/database/postgre"
 	"github.com/arseniizyk/mgkct-schedule-bot/services/scraper/pkg/parser"
 	"github.com/arseniizyk/mgkct-schedule-bot/services/scraper/pkg/server"
 	"google.golang.org/grpc"
 )
 
 type App struct {
-	cfg *config.Config
 	lis net.Listener
-	db  database.DatabaseRepository
+	db  *database.Database
 }
 
 func New() (*App, error) {
-	cfg, err := config.New()
-	if err != nil {
+	if err := config.LoadEnv(); err != nil {
 		slog.Error("can't load cfg", "err", err)
 		return nil, err
 	}
 
-	lis, err := net.Listen("tcp", ":"+cfg.GRPCPort)
+	lis, err := net.Listen("tcp", ":"+os.Getenv("GRPC_PORT"))
 	if err != nil {
-		slog.Error("can't load gRPC listener", "err", err)
+		slog.Error("can't start net listener for GRPC", "err", err)
+		return nil, err
+	}
+
+	db, err := database.New()
+	if err != nil {
+		slog.Error("can't connect to database", "err", err)
 		return nil, err
 	}
 
 	return &App{
-		cfg: cfg,
 		lis: lis,
-		db:  initDB(),
+		db:  db,
 	}, nil
 }
 
@@ -57,20 +58,20 @@ func (a *App) Run() error {
 	defer cancel()
 
 	parserObj := parser.New()
-	parserUC := parserUC.NewParserUsecase(a.db, parserObj)
-
-	scheduleUC := scheduleUC.NewScheduleUsecase(a.db)
+	scheduleRepo := scheduleRepo.New(a.db.Pool)
+	scheduleUC := scheduleUC.New(scheduleRepo)
+	parserUC := parserUC.New(scheduleUC, parserObj)
 
 	go func() {
-		ch := parserUC.GetScheduleEvery(ctx, 1*time.Minute)
-		for sch := range ch {
+		scheduleCh := parserUC.ParseScheduleEvery(ctx, 1*time.Minute)
+		for sch := range scheduleCh {
 			scheduleUC.SaveToCache(sch)
 			slog.Info("New schedule parsed and saved")
 		}
 	}()
 
 	grpcServer := grpc.NewServer()
-	httpSrv := server.NewHTTPServer(scheduleUC, a.cfg.HttpPort)
+	httpSrv := server.NewHTTPServer(scheduleUC)
 	a.startGRPC(scheduleUC, grpcServer)
 	httpSrv.Start()
 
@@ -95,32 +96,9 @@ func (a *App) startGRPC(schUC *scheduleUC.ScheduleUsecase, grpcServer *grpc.Serv
 	pb.RegisterScheduleServiceServer(grpcServer, server.NewGRPCServer(schUC))
 
 	go func() {
-		slog.Info("gRPC server started", "port", a.cfg.GRPCPort)
+		slog.Info("gRPC server started", "port", "GRPC_PORT")
 		if err := grpcServer.Serve(a.lis); err != nil {
 			slog.Error("gRPC serve error", "err", err)
 		}
 	}()
-}
-
-func initDB() database.DatabaseRepository {
-	url := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_DB"),
-		os.Getenv("POSTGRES_SSL"),
-	)
-
-	db, err := postgre.NewDatabase(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := db.Ping(context.Background()); err != nil {
-		log.Fatal(err)
-	}
-
-	return db
 }
