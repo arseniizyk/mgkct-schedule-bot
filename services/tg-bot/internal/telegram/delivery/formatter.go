ca9723c79e26579f40419c98bd18336e6bc02d32
arseniizyk <arseniizyk@gmail.com>
2025-09-15 22:04:16 +0300

    tg-bot: refactor: optimization + class formatting

--- services/tg-bot/internal/telegram/delivery/formatter.go
+++ services/tg-bot/internal/telegram/delivery/formatter.go
@@ -10,6 +10,7 @@ import (
 
 func formatScheduleDay(day *pb.Day) string {
 	var sb strings.Builder
+	sb.Grow(256)
 
 	sb.WriteString(fmt.Sprintf("*%s\n*", day.Name))
 	sb.WriteString(formatSubjects(day.Subjects))
@@ -20,6 +21,7 @@ func formatScheduleDay(day *pb.Day) string {
 
 func formatScheduleWeek(resp *pb.GroupScheduleResponse) string {
 	var sb strings.Builder
+	sb.Grow(len(resp.Day) * 128)
 
 	for _, day := range resp.Day {
 		sb.WriteString(formatScheduleDay(day))
@@ -30,46 +32,54 @@ func formatScheduleWeek(resp *pb.GroupScheduleResponse) string {
 
 func formatSubjects(subjects []*pb.Subject) string {
 	var sb strings.Builder
+	sb.Grow(len(subjects) * 80)
 
 	lastSubject := -1
-	for i, s := range subjects {
-		if !s.Empty {
+	for i := len(subjects) - 1; i >= 0; i-- {
+		if !subjects[i].Empty {
 			lastSubject = i
+			break
 		}
 	}
 
 	for i, subject := range subjects {
 		if subject.Empty {
 			if i > lastSubject {
-				continue
+				break
 			}
 			sb.WriteString(fmt.Sprintf("%d: --\n", i+1))
+			continue
 		}
 
-		if len(subject.Pairs) == 1 && !unicode.IsDigit(rune(subject.Pairs[0].Name[0])) {
-			pair := subject.Pairs[0]
-			sb.WriteString(fmt.Sprintf("%d: %s | %s | %s", i+1, pair.Name, pair.Type, pair.Teacher))
-			if pair.Class != "-" {
-				sb.WriteString(fmt.Sprintf(" | %s", pair.Class))
-			}
-			sb.WriteString("\n")
+		pairs := subject.Pairs
 
-		} else {
-			sb.WriteString(fmt.Sprintf("%d:\n", i+1))
-			for j, pair := range subject.Pairs {
-				if j == len(subject.Pairs)-1 {
-					sb.WriteString("└── ")
-				} else {
-					sb.WriteString("├── ")
-				}
-				sb.WriteString(fmt.Sprintf("%s | %s | %s", pair.Name, pair.Type, pair.Teacher))
-				if pair.Class != "-" {
-					sb.WriteString(fmt.Sprintf(" | %s", pair.Class))
-				}
-				sb.WriteString("\n")
+		if len(pairs) == 1 && !unicode.IsDigit(rune(pairs[0].Name[0])) { // If only 1 pair in subject and starts with digit
+			p := pairs[0]
+			sb.WriteString(fmt.Sprintf("%d: %s | %s | %s", i+1, p.Name, p.Type, p.Teacher))
+			sb.WriteString(formatClass(p.Class))
+			sb.WriteString("\n")
+			continue
+		}
+
+		sb.WriteString(fmt.Sprintf("%d:\n", i+1))
+		for j, p := range pairs {
+			if j == len(pairs)-1 { // if last pair in subject
+				sb.WriteString("└── ")
+			} else {
+				sb.WriteString("├── ")
 			}
+			sb.WriteString(fmt.Sprintf("%s | %s | %s", p.Name, p.Type, p.Teacher))
+			sb.WriteString(formatClass(p.Class))
+			sb.WriteString("\n")
 		}
 	}
 
 	return sb.String()
+}
+
+func formatClass(class string) string {
+	if class != "-" {
+		return " | " + class
+	}
+	return ""
 }
