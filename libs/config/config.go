8b29742d4396263a1f67a374db10a1413022ec81
Arsenii <arseniizyk@gmail.com>
2026-01-26 22:56:43 +0300

    refactor: libs/config .env updates

--- libs/config/config.go
+++ libs/config/config.go
@@ -1,29 +1,54 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
-	"github.com/kelseyhightower/envconfig"
+	"fmt"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	BotToken string `envconfig:"TELEGRAM_TOKEN"`
+	Scraper ScraperConfig
+	Bot     BotConfig
+	Nats    NatsConfig
+}
 
-	ScraperURL string `default:"scraper:9001" envconfig:"SCRAPER_URL"`
-	NatsURL    string `default:"nats://nats:4222" envconfig:"NATS_URL"`
+type ScraperConfig struct {
+	URL string `env:"SCRAPER_URL" env-default:"scraper:9001"`
 
-	PostgresPassword string `default:"password" envconfig:"POSTGRES_PASSWORD"`
-	PostgresUser     string `default:"postgres" envconfig:"POSTGRES_USER"`
-	PostgresDB       string `default:"postgres" envconfig:"POSTGRES_DB"`
-	PostgresSSL      string `default:"disable" envconfig:"POSTGRES_SSL"`
-	PostgresPort     string `default:"5432" envconfig:"POSTGRES_PORT"`
+	DB PostgresConfig `env-prefix:"SCRAPER_DB_"`
+}
+
+type BotConfig struct {
+	Token string `env:"TELEGRAM_TOKEN"`
+
+	DB PostgresConfig `env-prefix:"BOT_DB_"`
+}
+
+type NatsConfig struct {
+	URL string `env:"NATS_URL" env-default:"nats://nats:4222"`
+}
+
+type PostgresConfig struct {
+	Host     string `env:"HOST" env-required:"true"`
+	Port     string `env:"PORT" env-default:"5432"`
+	User     string `env:"USER" env-default:"postgres"`
+	Password string `env:"PASSWORD" env-default:"password"`
+	DBName   string `env:"NAME" env-required:"true"`
 }
 
 func New(configPath string) (*Config, error) {
+	const op = "libs.config.New"
 	var cfg Config
 
-	_ = godotenv.Load(configPath)
-	if err := envconfig.Process("", &cfg); err != nil {
-		return nil, err
+	if configPath == "" {
+		if err := cleanenv.ReadEnv(&cfg); err != nil {
+			return nil, fmt.Errorf("%s: failed to load from env: %s", op, err)
+		}
+		return &cfg, nil
+	}
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		return nil, fmt.Errorf("%s: failed to load from file: %s", op, err)
 	}
 
 	return &cfg, nil
--- libs/config/go.mod
+++ libs/config/go.mod
@@ -2,7 +2,11 @@ module github.com/arseniizyk/mgkct-schedule-bot/libs/config
 
 go 1.25.0
 
+require github.com/ilyakaznacheev/cleanenv v1.5.0
+
 require (
-	github.com/joho/godotenv v1.5.1
-	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/BurntSushi/toml v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
